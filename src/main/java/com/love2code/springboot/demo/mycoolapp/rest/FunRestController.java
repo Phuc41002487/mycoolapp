12d20e2f3c3c32411cfb2e0def4f81634a220286
Vo Thanh Phuc (AS-ER/ESP3) <ThanhPhuc.Vo@bosch.com>
2023-04-01 07:02:42 +0200

    Spring Boot Custom Properties + Disable Security for easier testing

--- src/main/java/com/love2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/love2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,24 @@
 package com.love2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+    // inject properties
+    @Value("${mylover.name}")
+    private String loverName;
+
+    @Value("${mylover.age}")
+    private String loverAge;
+
+    // expose new endpoint for my lover
+    @GetMapping("/lover")
+    public String getLoverInfo() {
+        return "My lover is " + loverName + " and her age is " + loverAge;
+    }
+
     // expose "/" that return "Hello World"
     @GetMapping("/")
     public String sayHello() {
